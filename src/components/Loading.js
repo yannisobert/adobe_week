9efd6f999d8cf1f06b6adb79bb49ec5cf8557ef9
Pierre CAILLET <pierre.caillet@edu.devinci.fr>
2022-11-15 16:57:05 +0100

    Trying to add gsap and loader

---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function Loading() { 
+    return (
+        <div className='loading'>
+            <img src="https://logos-marques.com/wp-content/uploads/2022/07/Cyberpunk-2077-Logo.png" alt="Logo"/>
+        </div>
+    );
+}
+
+
+
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import Loading from './components/Loading';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Loading />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
