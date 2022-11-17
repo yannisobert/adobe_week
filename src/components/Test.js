7e06a6ca7fa7294dcabb41a7bfb5e142f1472f29
yannisobert <90550556+yannisobert@users.noreply.github.com>
2022-11-17 14:34:45 +0100

    Big update

---
+++ src/components/Test.js
@@ -0,0 +1,9 @@
+export default function Team() {
+    return (
+        <div className='team'>
+            <div className="person">
+                <img src="../../public/img/portraitbis.png" alt="Image of a member of the team" />
+            </div>
+        </div>
+    );
+}
