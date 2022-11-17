1185f6398b960f364d8a40815c77b36cb2ccd385
Pierre CAILLET <pierre.caillet@edu.devinci.fr>
2022-11-17 14:50:01 +0100

    :lipstick:

---
+++ src/components/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+export default function Loading() { 
+    return (
+        <div className='loading'>
+            <img src="/assets/imgs/logo_e-lift.svg" alt="Logo"/>
+        </div>
+    );
+}
+
+
+
---
+++ src/components/SlideNav.js
@@ -0,0 +1,25 @@
+export default function SlideNav() {
+    return (
+    <nav className="slide-nav">
+        <div className="ul-color">
+            <ul>
+                <li>
+                    <img src="https://www.iim.fr/ecole-web/wp-content/uploads/2021/06/crea.png" alt="icon of the CDEB specialisation" /> <span>Création Design</span> 
+                </li>
+                <li>
+                    <img src="https://www.iim.fr/ecole-web/wp-content/uploads/2021/06/dev.png" alt="icon of the CDEB specialisation" /> <span>Coding</span>
+                </li>
+                <li>
+                    <img src="https://www.iim.fr/ecole-web/wp-content/uploads/2021/06/3dsvg.png" alt="icon of the CDEB specialisation" /> <span>Animation 3D</span>
+                </li>
+                <li>
+                    <img src="https://www.iim.fr/ecole-web/wp-content/uploads/2021/06/jv.png" alt="icon of the CDEB specialisation" /> <span>Jeux Vidéos</span>
+                </li>
+                <li>
+                    <img src="https://www.iim.fr/ecole-web/wp-content/uploads/2021/06/com.png" alt="icon of the CDEB specialisation" /> <span>Communication</span>
+                </li>
+            </ul>
+        </div>
+    </nav>
+)
+}
